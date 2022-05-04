58e1c1e9f17503474450afa5930bfe7701ac7a3e
AndrewShilin23 <103073144+AndrewShilin23@users.noreply.github.com>
2022-05-04 23:26:46 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+from pixellib.instance import instance_segmentation
+
+
+def object_detection_on_an_image():
+    segment_image = instance_segmentation()
+    segment_image.load_model("/wsl$/Ubuntu-20.04/root/mask_rcnn_coco.h5")
+
+    #target_class = segment_image.select_target_classes(person=True)
+
+    result = segment_image.segmentImage(
+        image_path="images.jpg",
+        # image_path="2cars_people.jpeg",
+        #image_path="3silicon_valley.jpg",
+        # show_bboxes=True,
+        #segment_target_classes=target_class,
+        # extract_segmented_objects=True,
+        # save_extracted_objects=True,
+        output_image_name="output.jpg"
+    )
+
+    print(result[0]["scores"])
+    objects_count = len(result[0]["scores"])
+    if objects_count > 1:
+        print(f"Объектов: {objects_count}")
+        found = True
+        print(found)
+    else:
+        print(f"Объектов: {objects_count}")
+        found = False
+        print(found)       
+
+    print(f"Объектов: {objects_count}")
+
+
+def main():
+    object_detection_on_an_image()
+
+
+if __name__ == '__main__':
+    main()

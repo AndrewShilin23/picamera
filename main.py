ef4b92414ea0388b6fe3958221401f242ccc2264
Andrew05 <andrew05shilin@gmail.com>
2022-05-15 22:05:46 +0300

    раскомментировал старый код

--- main.py
+++ main.py
@@ -6,27 +6,29 @@ def object_detection_on_an_image(image, model):
     segment_image = semantic_segmentation()
     segment_image.load_ade20k_model("weights/" + model)
 
-    # target_class = segment_image.select_target_classes(person=True)
+    target_class = segment_image.select_target_classes(person=True)
 
     result = segment_image.segmentAsAde20k(
         image_path="images/" + image,
-        # show_bboxes=True,
+        show_bboxes=True,
         # segment_target_classes=target_class,
         # extract_segmented_objects=True,
         # save_extracted_objects=True,
         output_image_name=image
     )
 
-    # print(result[0]["scores"])
-    # objects_count = len(result[0]["scores"])
-    # if objects_count > 1:
-    #     print(f"Объектов: {objects_count}")
-    #     found = True
-    #     print(found)
-    # else:
-    #     print(f"Объектов: {objects_count}")
-    #     found = False
-    #     print(found)       
+    print(result[0]["scores"])
+    objects_count = len(result[0]["scores"])
+    if objects_count > 1:
+        print(f"Объектов: {objects_count}")
+        found = True
+        print(found)
+    else:
+        print(f"Объектов: {objects_count}")
+        found = False
+        print(found)       
+    
+
 
     # print(f"Объектов: {objects_count}")
 
